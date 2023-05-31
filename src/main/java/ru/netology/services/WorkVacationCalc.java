a0968b68c253a799b010b464d44fa8c775243907
Anna Omelchenko <anna.g.omelchenko@gmail.com>
2023-05-31 06:53:43 +0300

    Parameterized test is added

--- src/main/java/ru/netology/services/WorkVacationCalc.java
+++ src/main/java/ru/netology/services/WorkVacationCalc.java
@@ -1,7 +1,7 @@
 package ru.netology.services;
 
 public class WorkVacationCalc {
-    public int calculateVacation (int income, int expenses, int threshold) {
+    public int calculateVacation(int income, int expenses, int threshold) {
         int count = 0; // подсчет количества отпусков в штуках
         int account = 0; // остаток на счете
         for (int month = 0; month < 12; month++) // количество итераций 12 раз в году
--- src/test/java/ru/netology/services/WorkVacationCalcTest.java
+++ src/test/java/ru/netology/services/WorkVacationCalcTest.java
@@ -1,24 +1,16 @@
 package ru.netology.services;
-//import ru.netology.services.WorkVacationCalc;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class WorkVacationCalcTest {
 
-    @Test
-    public void testVacationCalcLowSalary() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/inputs_vacation.csv")
+    public void testVacationCalcLowSalary(int expected, int income, int expenses, int threshold) {
         WorkVacationCalc service = new WorkVacationCalc();
-        int expected = 3;
-        int actual = service.calculateVacation(10000, 3000, 20000);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testVacationCalcHighSalary() {
-        WorkVacationCalc service = new WorkVacationCalc();
-        int expected = 2;
-        int actual = service.calculateVacation(100000, 60000, 150000);
+        int actual = service.calculateVacation(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
